15a7d328fd34c430a531ff6da5f08de0ff9fdf9b
ObertiFabio <101709153+ObertiFabio@users.noreply.github.com>
2023-05-09 08:02:23 +0000

    ciao

--- src/main/java/it/paleocapa/mastroiannim/JavaBossBot.java
+++ src/main/java/it/paleocapa/mastroiannim/JavaBossBot.java
@@ -6,8 +6,8 @@ import org.springframework.stereotype.Service;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
-import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
-
+import org.telegram.telegrambots.meta.exceptions.*;
+import java.util.*;
 
 @Service
 public class JavaBossBot extends TelegramLongPollingBot {
@@ -43,19 +43,95 @@ public class JavaBossBot extends TelegramLongPollingBot {
 
 	@Override
 	public void onUpdateReceived(Update update) {
-		if (update.hasMessage() && update.getMessage().hasText()) {
+
+		LOG.info("onUpdateReceived");
+		String messaggio = update.getMessage().getText();
+		String chatId = update.getMessage().getChatId().toString();
+		SendMessage invioMessaggio = new SendMessage();
+		invioMessaggio.setChatId(chatId);
+
+		LinkedList<String> listaCibo = new LinkedList<String>();
+		listaCibo.add("paneCotoletta");
+		listaCibo.add("focacciaProsciutto");
+		listaCibo.add("panzerotto");
+		
+		LinkedList<String> listaBere = new LinkedList<String>();
+		listaBere.add("acquaNaturale");
+		listaBere.add("cocaCola");
+		listaBere.add("ThePesca");
+
+		//boolean statoOrdinazione = false;
+
+		LinkedList<String> listaOrdinazioni = new LinkedList<String>();
+		listaOrdinazioni.add("ciao");
+		if(messaggio.equals("/listaCibo")){
+			invioMessaggio.setText(
+				"listaCibo: \n paneCotoletta $2 \n focacciaProsciutto $2.50 \n panzerotto $1.50" 
+			);
+		}
+		else if(messaggio.equals("/listaBere")){
+			invioMessaggio.setText(
+				"listaBere: \n acquaNaturale $1 \n cocaCola $2 \n ThePesca $1.50" 
+			);
+		}
+		/* 
+		else if(messaggio.equals("/Ordinazioni")){
+			invioMessaggio.setText(
+				"scrivi tutti i prodotti di cui hai bisogno..."
+			);
+			listaOrdinazioni.add(messaggio);
 			
-			long chatId = update.getMessage().getChatId();
-			
-			SendMessage message = new SendMessage();
-			message.setChatId(chatId);
-			message.setText("Benvenuto! Come posso aiutarti?");
-			
-			try {
-				execute(message);
-			} catch (TelegramApiException e) {
-				LOG.error(e.getMessage());
+			Iterator<String> iteratoreCibo = listaCibo.iterator();
+			Iterator<String> iteratoreBere = listaBere.iterator();
+			while (iteratoreCibo.hasNext()) {
+				String element = iteratoreCibo.next();
+				if (element.equals(messaggio)) {
+					listaOrdinazioni.add(messaggio);
+				}
+				
+				else{
+					invioMessaggio.setText("prodotto non disponibile...");
+				}
+				
 			}
+			while (iteratoreBere.hasNext()) {
+				String element = iteratoreBere.next();
+				if (element.equals(messaggio)) {
+					listaOrdinazioni.add(messaggio);
+				}
+				/* 
+				else{
+					invioMessaggio.setText("prodotto non disponibile...");
+				}
+				
+			}
+			
+		}
+		*/
+		else if(messaggio.equals("/stampaOrdinazioni")){
+			if(listaOrdinazioni.size() <= 0){
+				invioMessaggio.setText("lista vuota");
+			}
+			Iterator<String> iterator = listaCibo.iterator();
+			StringBuilder stringBuilder = new StringBuilder();
+			while (iterator.hasNext()) {
+				String element = iterator.next();
+				stringBuilder.append(element);
+				if (iterator.hasNext()) {
+					stringBuilder.append(", ");
+				}
+			}
+			LOG.info(stringBuilder.toString());
+		}
+		
+		
+
+		try {
+			execute(invioMessaggio);
+		} catch (TelegramApiException e) {
+			LOG.error(e.getMessage());
 		}
 	}
+		
 }
+
