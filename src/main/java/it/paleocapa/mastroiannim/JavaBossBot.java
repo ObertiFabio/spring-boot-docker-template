845230f59fbf2c3927aa094aaf7d9171bb30581d
ObertiFabio <101709153+ObertiFabio@users.noreply.github.com>
2023-05-23 06:46:05 +0000

    ordinazione funzionante

--- src/main/java/it/paleocapa/mastroiannim/JavaBossBot.java
+++ src/main/java/it/paleocapa/mastroiannim/JavaBossBot.java
@@ -41,96 +41,81 @@ public class JavaBossBot extends TelegramLongPollingBot {
 		return botUsername;
 	}
 
+	LinkedList<String> listaOrdinazioni = new LinkedList<String>();
+	
 	@Override
 	public void onUpdateReceived(Update update) {
 
+		
 		LOG.info("onUpdateReceived");
 		String messaggio = update.getMessage().getText();
 		String chatId = update.getMessage().getChatId().toString();
 		SendMessage invioMessaggio = new SendMessage();
 		invioMessaggio.setChatId(chatId);
-
+	
 		LinkedList<String> listaCibo = new LinkedList<String>();
 		listaCibo.add("paneCotoletta");
 		listaCibo.add("focacciaProsciutto");
 		listaCibo.add("panzerotto");
-		
+		listaCibo.add("speck-brie");
+		listaCibo.add("hamburger");
+		listaCibo.add("calzone");
+	
 		LinkedList<String> listaBere = new LinkedList<String>();
 		listaBere.add("acquaNaturale");
 		listaBere.add("cocaCola");
 		listaBere.add("ThePesca");
-
-		//boolean statoOrdinazione = false;
-
-		LinkedList<String> listaOrdinazioni = new LinkedList<String>();
-		listaOrdinazioni.add("ciao");
-		if(messaggio.equals("/listaCibo")){
+	
+	
+		if (messaggio.equals("/cibo")) {
 			invioMessaggio.setText(
-				"listaCibo: \n paneCotoletta $2 \n focacciaProsciutto $2.50 \n panzerotto $1.50" 
+				"listaCibo: \n paneCotoletta $2 \n focacciaProsciutto $2.50 \n panzerotto $1.50 \n speck-brie $2.50 \n hamburger $2 \n calzone $2"
 			);
-		}
-		else if(messaggio.equals("/listaBere")){
+		} else if (messaggio.equals("/bibite")) {
 			invioMessaggio.setText(
-				"listaBere: \n acquaNaturale $1 \n cocaCola $2 \n ThePesca $1.50" 
+				"listaBere: \n acquaNaturale $1 \n cocaCola $2 \n ThePesca $1.50"
 			);
-		}
-		/* 
-		else if(messaggio.equals("/Ordinazioni")){
+		} else if (messaggio.equals("/start")) {
 			invioMessaggio.setText(
-				"scrivi tutti i prodotti di cui hai bisogno..."
+				"Buongiorno, come posso aiutarti?"
 			);
-			listaOrdinazioni.add(messaggio);
-			
-			Iterator<String> iteratoreCibo = listaCibo.iterator();
-			Iterator<String> iteratoreBere = listaBere.iterator();
-			while (iteratoreCibo.hasNext()) {
-				String element = iteratoreCibo.next();
-				if (element.equals(messaggio)) {
-					listaOrdinazioni.add(messaggio);
-				}
-				
-				else{
-					invioMessaggio.setText("prodotto non disponibile...");
-				}
-				
-			}
-			while (iteratoreBere.hasNext()) {
-				String element = iteratoreBere.next();
-				if (element.equals(messaggio)) {
-					listaOrdinazioni.add(messaggio);
-				}
-				/* 
-				else{
-					invioMessaggio.setText("prodotto non disponibile...");
-				}
-				
-			}
-			
+		} else if (messaggio.equals("/ordinacibo")) {
+			invioMessaggio.setText("scrivi tutti i prodotti di cui hai bisogno...");
 		}
-		*/
-		else if(messaggio.equals("/stampaOrdinazioni")){
-			if(listaOrdinazioni.size() <= 0){
+		 else if (messaggio.equals("/stampaordinazioni")) {
+			if (listaOrdinazioni.size() <= 0) {
 				invioMessaggio.setText("lista vuota");
+			} else {
+				StringBuilder stringBuilder = new StringBuilder();
+				for (String elemento : listaOrdinazioni) {
+					stringBuilder.append(elemento).append(", ");
+				}
+				stringBuilder.setLength(stringBuilder.length() - 2); // Rimuove l'ultima virgola e lo spazio
+				invioMessaggio.setText(stringBuilder.toString());
 			}
-			Iterator<String> iterator = listaCibo.iterator();
-			StringBuilder stringBuilder = new StringBuilder();
-			while (iterator.hasNext()) {
-				String element = iterator.next();
-				stringBuilder.append(element);
-				if (iterator.hasNext()) {
-					stringBuilder.append(", ");
+		} else {
+			boolean prodottoDisponibile = false;
+	
+			for (String elemento : listaCibo) {
+				if (elemento.equals(messaggio)) {
+					listaOrdinazioni.add(messaggio);
+					invioMessaggio.setText("prodotto aggiunto");
+					prodottoDisponibile = true;
+					break;
 				}
 			}
-			LOG.info(stringBuilder.toString());
+	
+			if (!prodottoDisponibile) {
+				invioMessaggio.setText("prodotto non disponibile...");
+			}
 		}
-		
-		
-
+	
 		try {
 			execute(invioMessaggio);
 		} catch (TelegramApiException e) {
 			LOG.error(e.getMessage());
 		}
+		
 	}
 		
 }
